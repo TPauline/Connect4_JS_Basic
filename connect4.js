03208d442cd0a4fab7c6cf069cc687c31e289e92
TPauline <73195686+TPauline@users.noreply.github.com>
2022-11-24 02:06:19 -0500

    11/24/22 - 2:06pm

--- connect4.js
+++ connect4.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    gameMode = null
+    gameMode = "computer"
     if (localStorage.gameMode) {
         gameMode = localStorage.gameMode
     } else {
@@ -7,21 +7,58 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     console.log(gameMode)
 
+    pieces = { redUrl: "url(images/redPiece.png)", yellowUrl: "url(images/yellowPiece.png)" }
     board = document.querySelector("#board")
-    const overhead_cicles = document.getElementById('over_board').children;
     board_overlay = document.querySelector("#board_overlay")
-    pieces = { red: "url(images/redPiece.png)", yellow: "url(images/yellowPiece.png)" }
+    playerInfo1 = document.querySelector("#playerInfo1")
+    playerInfo2 = document.querySelector("#playerInfo2")
     circles = []
     circle = document.querySelector(".circle")
-    // console.log(board.offsetLeft, board.offsetTop)
+    const overhead_cicles = document.getElementById('over_board').children;
 
     function shuffle(arr) {
         arr.sort(() => Math.random() - 0.5);
         return arr
     }
-    temp = shuffle([pieces.red, pieces.yellow])
-    player1 = { numPieces: 24, color: temp[0], turn: true }
-    player2 = { numPieces: 24, color: temp[1], turn: false, mode:gameMode }
+    temp = shuffle([{ url: "url(images/redPiece.png)", class: "redPiece" }, { url: "url(images/yellowPiece.png)", class: "yellowPiece" }])
+    player1 = { numPieces: 24, url: temp[0].url, class: temp[0].class, turn: true }
+    player2 = { numPieces: 24, url: temp[1].url, class: temp[1].class, turn: false }
+    const overlay_cols = []
+
+    function playerInfo_setup(playerInfo, player, n) {
+        let c_over = document.createElement("div")
+        let label = document.createElement("button")
+        label.id = "label"
+        label.innerText = "Player" + n
+        c_over.classList.add("circle")
+        c_over.classList.add(player.class)
+        playerInfo.appendChild(c_over)
+        playerInfo.appendChild(label)
+    }
+
+    function playerInfo_update(playerInfo, player, n) {
+
+        let label = playerInfo.querySelector("button")
+        // console.log(">>>", gameMode)
+
+        if (player.turn) {
+            if (gameMode == "computer" && player == player2) {
+                label.innerText = "Computer's Turn"
+            } else {
+                label.innerText = "Player" + n + "'s Turn"
+            }
+
+        } else {
+            if (gameMode == "computer" && player == player2) {
+                label.innerText = "Computer"
+            } else {
+                label.innerText = "Player" + n
+            }
+
+        }
+
+    }
+
 
     function create_GameBoard() {
         board.style.top = board.offsetTop + 120 + "px"
@@ -37,159 +74,163 @@ document.addEventListener('DOMContentLoaded', () => {
             over_board.appendChild(c_over)
         }
 
-
+        let num = 0;
         for (let r = 0; r < 6; r++) {
             row = []
             for (let c = 0; c < 7; c++) {
-                clone = circle.cloneNode("true")
-                board.appendChild(clone)
-                row.push(clone)
+                if (num < 42) {
+                    clone = circle.cloneNode("true")
+                    board.appendChild(clone)
+                    row.push(clone)
+                }
+
             }
             circles.push(row)
+            num++
         }
+
 
         board.removeChild(circle)
-    }
 
-function drop(row, col, end , color){
-    if (color){
-        color = false
-    }else{
-        color =true
     }
-    
-    console.log(row, color)
-    if( row < end ){
-        if (color){
-            circles[row][col].style.backgroundImage  = player1.color
-           
-        }else{
-            circles[row][col].style.backgroundImage  = "none"
-            
-            row++
-        }
-
-    }else if (row == end){
-        circles[row][col].style.backgroundImage  = player1.color
-        end--
-    }
-}
 
     function setup_board_overlay() {
-        console.log(overhead_cicles)
+        // console.log(overhead_cicles)
 
         for (let c = 0; c < 7; c++) {
-            let col_ovly = document.createElement("div")
+            col_ovly = document.createElement("div")
             col_ovly.classList.add("column_overlay")
-            col_ovly.addEventListener('mouseover',() =>{
+            col_ovly.addEventListener('mouseover', () => {
                 const circle = overhead_cicles.item(c);
-                console.log(circle)
-                if (player1.turn){
-                    circle.style.backgroundImage  = player1.color
-                }else{
-                    circle.style.backgroundImage  = player2.color
+                // console.log(circle)
+                if (player1.turn) {
+                    circle.style.backgroundImage = player1.url
+                } else {
+                    circle.style.backgroundImage = player2.url
                 }
             })
 
-            col_ovly.addEventListener('mouseout',() =>{
+            col_ovly.addEventListener('mouseout', () => {
                 const circle = overhead_cicles.item(c);
-                console.log(circle)
-                if (player1.turn){
-                    circle.style.backgroundImage  = "none"
-                }else{
-                    circle.style.backgroundImage  = "none"
+                // console.log(circle)
+                if (player1.turn) {
+                    circle.style.backgroundImage = "none"
+                } else {
+                    circle.style.backgroundImage = "none"
                 }
             })
-
-            col_ovly.addEventListener('click',() =>{
-                const circle = overhead_cicles.item(c);
-                console.log(circle)
-                if (player1.turn){
-
-                    circle.style.backgroundImage  = "none"
-                    row = 0
-                    end = 5
-                    color = true
-                    console.log(circles)
-                    console.log("____",circles[0][c])
-                    
-
-                    setInterval(() =>{  
-                        drop(row,c,end,color);
-                    }, 1000)
-
-                    // while ( row < 6  ){ //&& circles[row][c].style.backgroundImage  != player1.color
-                    //     console.log(row)
-                    //     circles[row][c].style.backgroundImage  = player1.color
-                    //     // setTimeout(alert("4 seconds"),1000); 
-                    //     // // circles[row][c].style.backgroundImage  ="none"
-                    //     // setTimeout(alert("4 seconds"),1000); 
-                    //     row++  
-                        
-                    // }
-                    
-                }else{
-                    
-                }
-            })
-
-
             board_overlay.appendChild(col_ovly)
         }
 
+
+        let idx = 0
+        for (const element of document.getElementById('board_overlay').children) {
+            overlay_cols.push({ column: element, index: idx, columnList: [], shouldColor: 6 },)
+            colobj = overlay_cols[overlay_cols.length - 1]
+            for (let j = 0; j < 6; j++) {
+                // console.log("jjjjjj", colobj.index)
+                colobj.columnList.push(circles[j][colobj.index])
+            }
+            idx++
+        }
+    }
+
+    function colClicks() {
+        for (const colobj of overlay_cols) {
+            // {column:element, index:0, columnList:[]}
+            colobj.column.addEventListener('click', () => {
+                player = null
+                if (player1.turn) {
+                    player = player1
+                } else {
+                    player = player2
+                }
+
+                if (gameMode == "computer" && player == player2) {
+                    alert("Computer's  Turn")
+
+                } else {
+
+                    console.log("jjjjjj", colobj.index, colobj.shouldColor)
+                    colobj.shouldColor--;
+                    if (colobj.shouldColor >= 0 && !colobj.columnList[colobj.shouldColor].classList.contains(player.class)) {
+                        colobj.columnList[colobj.shouldColor].classList.add(player.class)
+                    }
+                    player.numPieces--
+                    player1.turn = !player1.turn
+                    player2.turn = !player2.turn
+                }
+            })
+        }
     }
 
     create_GameBoard()
     setup_board_overlay()
+    playerInfo_setup(playerInfo1, player1, 1)
+    playerInfo_setup(playerInfo2, player2, 2)
 
 
 
+    setInterval(() => {
 
-    // circles[6][0].style.background_color = pieces.red
-
-
-    // console.log(circles)
-    // circles[5][0].style.backgroundImage = pieces.red
-    // console.log(circles[5][0])
-    // circles[5][6].style.backgroundImage = pieces.yellow
-    // console.log(circles[5][5])
+        playerInfo_update(playerInfo1, player1, 1)
+        playerInfo_update(playerInfo2, player2, 2)
 
 
+    }, 100)
 
+    setInterval(() => {
 
+        if (gameMode == "computer" && player2.turn) {
 
+            alert("****Computer's  Turn******")
 
+            if( player2.turn){
 
-    function blah(i, j, c1, c2) {
-        if (j % 2 == 0) {
-            circles[i][j].style.backgroundImage = c1
-        } else {
-            circles[i][j].style.backgroundImage = c2
-        }
-    }
-    function setup_toDelete_later() {
-        for (let i = 0; i < 6; i++) {
-
-            for (let j = 0; j < 7; j++) {
-                if (i % 2 == 0) {
-                    blah(i, j, pieces.red, pieces.yellow)
-                } else {
-                    blah(i, j, pieces.yellow, pieces.red)
+                let pos = Math.floor(Math.random() * 7)
+                console.log(pos)
+                const colobj = overlay_cols[pos];
+                console.log(colobj,player2.class)
+                if (colobj.shouldColor >= 0 && !colobj.columnList[colobj.pos].classList.contains(player2.class)
+                && !colobj.columnList[colobj.shouldColor].classList.contains(player1.class)) {
+                    colobj.columnList[colobj.shouldColor].classList.add(player2.class)
+                    player1.turn = !player1.turn
+                    player2.turn = !player2.turn
+                }else{
+                    console.log("computer is trying again")
                 }
+                player2.numPieces--
             }
+
+
+            
+          
         }
-    }
-    // setup_toDelete_later()
+
+    }, 1000)
 
 
-    // document.getElementById("vs_computer").addEventListener("click", () => {
-    //     localStorage.gameMode = "computer"
-    // });
 
+    colClicks()
 
-    // document.getElementById("vs_user").addEventListener("click", () => {
-    //     localStorage.gameMode = "user"
-    // });
+    // function blah(i, j, c1, c2) {
+    //     if (j % 2 == 0) {
+    //         circles[i][j].style.backgroundImage = c1
+    //     } else {
+    //         circles[i][j].style.backgroundImage = c2
+    //     }
+    // }
 
+    // function setup_toDelete_later() {
+    //     for (let i = 0; i < 6; i++) {
 
+    //         for (let j = 0; j < 7; j++) {
+    //             if (i % 2 == 0) {
+    //                 blah(i, j, pieces.redUrl, pieces.yellowUrl)
+    //             } else {
+    //                 blah(i, j, pieces.yellowUrl, pieces.redUrl)
+    //             }
+    //         }
+    //     }
+    // }
 })
